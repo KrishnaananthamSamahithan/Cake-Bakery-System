b822cc7568a32697028bceb8e790c542d6f07d28
Samahithan <krishnaanantham.20210863@iit.ac.lk>
2023-10-05 21:14:21 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+            
+            <div>
+                <strong>Copyright @</strong> Vijitha Cakes &copy; 2023 Developed by <a href="#" target="_blank">Samahithan</a>
+            </div>
+        </div>
---
+++ includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "root", "", "cake_shop_db");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
